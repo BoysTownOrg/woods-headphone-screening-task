59184f8ee4ab35ceeca3a21002e73bf7b28f4e10
Seth Bashford <seth.bashford@protonmail.com>
2021-09-10 15:43:21 -0500

    adding example

---
+++ jsPsych/main.js
@@ -0,0 +1,43 @@
+import * as toneGeneration from "../lib/tone-generation.js";
+
+jsPsych.plugins["play-tone"] = {
+  info: {},
+  trial(displayElement, trialParameters) {
+    const audioContext = jsPsych.pluginAPI.audioContext();
+    const audioBuffer = audioContext.createBuffer(1, 44100, 44100);
+    const channel = audioBuffer.getChannelData(0);
+    const parameters = {
+      sampleRate_Hz: 44100,
+      frequency_Hz: 200,
+      duration_ms: 1000,
+    };
+    const ramp = toneGeneration.ramp(parameters);
+    const tone = toneGeneration.multiplyFront(
+      toneGeneration.multiplyBack(
+        toneGeneration.pure(parameters),
+        ramp.reverse()
+      ),
+      ramp
+    );
+    for (let i = 0; i < channel.length; i += 1) channel[i] = tone[i];
+    const audioSource = audioContext.createBufferSource();
+    audioSource.buffer = audioBuffer;
+    audioSource.connect(audioContext.destination);
+    while (displayElement.firstChild) {
+      displayElement.removeChild(displayElement.lastChild);
+    }
+    const playButton = document.createElement("button");
+    playButton.onclick = () => {
+      audioSource.start();
+    };
+    playButton.textContent = "play";
+    displayElement.append(playButton);
+    const exitButton = document.createElement("button");
+    exitButton.onclick = () => {
+      jsPsych.finishTrial();
+    };
+    exitButton.textContent = "exit";
+    displayElement.append(exitButton);
+  },
+};
+jsPsych.init({ timeline: [{ type: "play-tone" }] });
