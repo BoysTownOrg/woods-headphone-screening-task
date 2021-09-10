3bfdcf290270c0a7c822e76e8cdec95bdc79fd6c
Seth Bashford <seth.bashford@protonmail.com>
2021-09-10 14:49:36 -0500

    another test

--- spec/tone-generation-spec.js
+++ spec/tone-generation-spec.js
@@ -1,19 +1,35 @@
-import { pure } from "../lib/tone-generation.js";
+import { pure, ramp } from "../lib/tone-generation.js";
+
+function squared(x) {
+  return x * x;
+}
 
 describe("tone generation", () => {
-  it("tbd", () => {
+  it("generates pure tones", () => {
     const tone = pure({
       sampleRate_Hz: 44100,
-      frequency_Hz: 440,
+      frequency_Hz: 200,
       duration_ms: 1000,
     });
     expect(tone.slice(0, 5)).toEqual([
-      Math.sin(2 * Math.PI * 440 * 0),
-      Math.sin((2 * Math.PI * 440 * 1) / 44100),
-      Math.sin((2 * Math.PI * 440 * 2) / 44100),
-      Math.sin((2 * Math.PI * 440 * 3) / 44100),
-      Math.sin((2 * Math.PI * 440 * 4) / 44100),
+      Math.sin(2 * Math.PI * 200 * 0),
+      Math.sin((2 * Math.PI * 200 * 1) / 44100),
+      Math.sin((2 * Math.PI * 200 * 2) / 44100),
+      Math.sin((2 * Math.PI * 200 * 3) / 44100),
+      Math.sin((2 * Math.PI * 200 * 4) / 44100),
     ]);
     expect(tone.length).toEqual(44100);
+  });
+
+  it("generates ramps", () => {
+    expect(
+      ramp({ sampleRate_Hz: 44100, duration_ms: 100 }).slice(0, 5)
+    ).toEqual([
+      squared(Math.sin((Math.PI * 0) / 4410)),
+      squared(Math.sin((Math.PI * 1) / 4410)),
+      squared(Math.sin((Math.PI * 2) / 4410)),
+      squared(Math.sin((Math.PI * 3) / 4410)),
+      squared(Math.sin((Math.PI * 4) / 4410)),
+    ]);
   });
 });
