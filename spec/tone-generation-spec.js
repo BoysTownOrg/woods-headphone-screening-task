d99a2048ed5c47054645412cf586b4c1cc572d08
Seth Bashford <seth.bashford@protonmail.com>
2021-09-10 14:19:48 -0500

    first test

--- spec/tone-generation-spec.js
+++ spec/tone-generation-spec.js
@@ -1,5 +1,19 @@
+import { pure } from "../lib/tone-generation.js";
+
 describe("tone generation", () => {
-    it("tbd", () => {
-        expect(true).toEqual(false);
-    });
+  it("tbd", () => {
+    expect(
+      pure({
+        sampleRate_Hz: 44100,
+        frequency_Hz: 440,
+        duration_ms: 1000,
+      }).slice(0, 5)
+    ).toEqual([
+      Math.sin(2 * Math.PI * 440 * 0),
+      Math.sin((2 * Math.PI * 440 * 1) / 44100),
+      Math.sin((2 * Math.PI * 440 * 2) / 44100),
+      Math.sin((2 * Math.PI * 440 * 3) / 44100),
+      Math.sin((2 * Math.PI * 440 * 4) / 44100),
+    ]);
+  });
 });
